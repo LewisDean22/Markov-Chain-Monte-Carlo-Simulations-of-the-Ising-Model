d028cf73d926cf7aa26a5f8b7404acea10e98383
rdef12 <126090366+rdef12@users.noreply.github.com>
2023-04-18 13:49:28 +0100

    Add files via upload

---
+++ wolff_algorithm_18_04/convergence_plotter.py
@@ -0,0 +1,247 @@
+# -*- coding: utf-8 -*-
+"""
+Title: __________
+------------------------------------------------------------------------
+                         description
+------------------------------------------------------------------------
+Created: Wed Mar  1 15:14:56 2023
+Author: Robin de Freitas
+UID: 10887826
+Contact: robin.defreitas@student.manchester.ac.uk
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import datetime
+from wolff_algorithm import find_new_state_from_cluster_point
+from new_state_generation import choose_spins_to_flip
+from initialise_model import initialise_ones_state, initialise_random_state
+from colorama import Style, Fore
+from print_formats import pvar
+from show_state import plot_state
+
+
+LENGTH = 50
+J_CONST = 1
+TEMP = 1.5
+NUMBER_OF_ITERATIONS = 50
+
+
+def get_save_confirmation():
+    while True:
+        user_input = input("Would you like to save this plot? [y/n]: ")
+        if user_input == "y":
+            return True
+        if user_input == "n":
+            return False
+        print(Style.BRIGHT + Fore.RED + "Please input y or n (or Ctrl+c to exit).")
+        print(Style.RESET_ALL)
+
+
+def get_high_or_low_expected_temperature():
+    while True:
+        user_input = input("Do you expect this temperature to be high or low?"
+                           "\n(If unsure enter high)"
+                           "\n[high/low]: ")
+        if user_input == "high":
+            return True
+        if user_input == "low":
+            return False
+        print(Style.BRIGHT + Fore.RED + "Please input high or low (or Ctrl+c to exit).")
+        print(Style.RESET_ALL)
+
+
+def plot_magnetisation_high_temp(loop_number, magnetisation, title, temperature, save, length=LENGTH):
+    fig, ax = plt.subplots()
+    now = datetime.datetime.now()
+    time_str = now.strftime("%H:%M:%S")
+    ax.plot(loop_number, magnetisation, color='red')
+    ax.set_ylim(-1.05,1.05)
+    ax.set_xlabel("Cluster Number")
+    ax.set_ylabel("Magnetisation")
+    ax.set_title("{} initialisation: {}, T = {} | {}".format(title, length, temperature, time_str))
+    if save:
+        plt.savefig("{}_initialisation_{}".format(title, length), dpi=600)
+    plt.show()
+    plt.close()
+
+def plot_magnetisation_low_temp(loop_number, magnetisation, title, temperature, save, length=LENGTH):
+    absolute_magnetisation = np.abs(magnetisation)
+    fig, ax = plt.subplots()
+    now = datetime.datetime.now()
+    time_str = now.strftime("%H:%M:%S")
+    ax.plot(loop_number, absolute_magnetisation, color='blue')
+    ax.set_ylim(-0.05,1.05)
+    ax.set_xlabel("Cluster Number")
+    ax.set_ylabel("Magnetisation")
+    ax.set_title("{} initialisation: {}, T = {} | {}".format(title, length, temperature, time_str))
+    if save:
+        plt.savefig("{}_initialisation_{}".format(title, length), dpi=600)
+    plt.show()
+    plt.close()
+
+
+# def plot_magnetisation_real_time_high_temp(loop_number, magnetisation, title, temperature, save, length=LENGTH):
+#     fig, ax = plt.subplots()
+
+#     # animated=True tells matplotlib to only draw the artist when we
+#     # explicitly request it
+#     (ln,) = ax.plot(loop_number, magnetisation, animated=True)
+    
+#     # make sure the window is raised, but the script keeps going
+#     plt.show(block=False)
+    
+#     # stop to admire our empty window axes and ensure it is rendered at
+#     # least once.
+#     #
+#     # We need to fully draw the figure at its final size on the screen
+#     # before we continue on so that :
+#     #  a) we have the correctly sized and drawn background to grab
+#     #  b) we have a cached renderer so that ``ax.draw_artist`` works
+#     # so we spin the event loop to let the backend process any pending operations
+#     plt.pause(0.1)
+    
+#     # get copy of entire figure (everything inside fig.bbox) sans animated artist
+#     bg = fig.canvas.copy_from_bbox(fig.bbox)
+#     # draw the animated artist, this uses a cached renderer
+#     ax.draw_artist(ln)
+#     # show the result to the screen, this pushes the updated RGBA buffer from the
+#     # renderer to the GUI framework so you can see it
+#     fig.canvas.blit(fig.bbox)
+    
+#     for j in range(100):
+#         # reset the background back in the canvas state, screen unchanged
+#         fig.canvas.restore_region(bg)
+#         # update the artist, neither the canvas state nor the screen have changed
+#         ln.set_ydata(np.sin(x + (j / 100) * np.pi))
+#         # re-render the artist, updating the canvas state, but not the screen
+#         ax.draw_artist(ln)
+#         # copy the image to the GUI state, but screen might not be changed yet
+#         fig.canvas.blit(fig.bbox)
+#         # flush any pending GUI events, re-painting the screen if needed
+#         fig.canvas.flush_events()
+#         # you can put a pause in if you want to slow things down
+        # plt.pause(.1)
+
+
+def main():
+    
+    save = get_save_confirmation()
+    high_temp = get_high_or_low_expected_temperature()
+
+    loop_range = range(1, NUMBER_OF_ITERATIONS+1, 1)
+    coordinates_to_flip = choose_spins_to_flip(LENGTH, NUMBER_OF_ITERATIONS)
+    # print(coordinates_to_flip)
+    
+    initial_state_random = initialise_random_state(LENGTH)
+    new_state_random = initial_state_random
+    # loop_range = []
+    normalised_magnetisation_random_list = []
+    # initial_state_ones = initialise_ones_state(LENGTH)
+    
+    
+    #####################################################
+        
+    # fig, ax = plt.subplots()
+
+    # # animated=True tells matplotlib to only draw the artist when we
+    # # explicitly request it
+    # (ln,) = ax.plot(loop_range, normalised_magnetisation_random_list, animated=True)
+    
+    # # make sure the window is raised, but the script keeps going
+    # plt.show(block=False)
+    
+    # # stop to admire our empty window axes and ensure it is rendered at
+    # # least once.
+    # #
+    # # We need to fully draw the figure at its final size on the screen
+    # # before we continue on so that :
+    # #  a) we have the correctly sized and drawn background to grab
+    # #  b) we have a cached renderer so that ``ax.draw_artist`` works
+    # # so we spin the event loop to let the backend process any pending operations
+    # plt.pause(0.1)
+    
+    # # get copy of entire figure (everything inside fig.bbox) sans animated artist
+    # bg = fig.canvas.copy_from_bbox(fig.bbox)
+    # # draw the animated artist, this uses a cached renderer
+    # ax.draw_artist(ln)
+    # # show the result to the screen, this pushes the updated RGBA buffer from the
+    # # renderer to the GUI framework so you can see it
+    # fig.canvas.blit(fig.bbox)
+    
+    ##################################################
+    
+    
+    for count, spin_position in enumerate(coordinates_to_flip):
+        
+        
+        #######################################################
+        
+        # fig.canvas.restore_region(bg)
+        
+        #######################################################
+        
+        
+        coordinate = (spin_position[0], spin_position[1])
+        new_state_random = find_new_state_from_cluster_point(new_state_random, coordinate, J_CONST, TEMP)
+        # plot_state(new_state_random, 'random {}'.format(count))
+
+        magnetisations_random = np.sum(new_state_random)
+        normalised_magnetisation_random = magnetisations_random / LENGTH**2
+        # loop_range += [count]
+        # ln.set_ydata(normalised_magnetisation_random)
+        normalised_magnetisation_random_list += [normalised_magnetisation_random]
+        # plot_state(new_state_random, "random | T = {}".format(TEMP))
+        print(Style.BRIGHT + Fore.RED + "RANDOM: {}".format(count))
+        print(Style.RESET_ALL)
+        
+        
+        #######################################################
+        
+        # ax.draw_artist(ln)
+        # # copy the image to the GUI state, but screen might not be changed yet
+        # fig.canvas.blit(fig.bbox)
+        # # flush any pending GUI events, re-painting the screen if needed
+        # fig.canvas.flush_events()
+        # # you can put a pause in if you want to slow things down
+        # # plt.pause(.1)
+
+    
+
+    initial_state_ones = initialise_ones_state(LENGTH)
+    new_state_ones = initial_state_ones
+    normalised_magnetisation_ones_list = []
+    for count, spin_position in enumerate(coordinates_to_flip):
+        coordinate = (spin_position[0], spin_position[1])
+        new_state_ones = find_new_state_from_cluster_point(new_state_ones, coordinate, J_CONST, TEMP)
+        magnetisations_ones = np.sum(new_state_ones)
+        normalised_magnetisation_ones = magnetisations_ones / LENGTH**2
+        normalised_magnetisation_ones_list += [normalised_magnetisation_ones]
+        print(Style.BRIGHT + Fore.RED + "ONES: {}".format(count))
+        print(Style.RESET_ALL)
+    
+    # new_state_ones, energies_ones, magnetisations_ones = find_new_state_from_cluster_point(initial_state_ones, TEMP, J_CONST, LENGTH, NUMBER_OF_ITERATIONS, NUMBER_OF_MC_LOOPS)
+    # normalised_magnetisation_ones = magnetisations_ones / NUMBER_OF_ITERATIONS
+    
+    if high_temp:
+        plot_magnetisation_high_temp(loop_range, normalised_magnetisation_random_list, "Random", TEMP, save)
+        plot_magnetisation_high_temp(loop_range, normalised_magnetisation_ones_list, "Ones", TEMP, save)
+        plot_state(new_state_random, "random | T = {}".format(TEMP))
+        pvar(normalised_magnetisation_random_list)
+        # pvar(normalised_magnetisation_ones_list)
+        # plot_state
+        return
+    
+    plot_magnetisation_low_temp(loop_range, normalised_magnetisation_random_list, "Random", TEMP, save)
+    # plot_magnetisation_low_temp(loop_range, normalised_magnetisation_ones_list, "Ones", TEMP, save)
+
+    plot_state(new_state_random, "random | T = {}".format(TEMP))
+
+    pvar(normalised_magnetisation_random_list)
+    # pvar(normalised_magnetisation_ones_list)
+
+    return 0
+
+
+if __name__ == "__main__":
+    main()
---
+++ wolff_algorithm_18_04/wolff_algorithm.py
@@ -0,0 +1,145 @@
+# -*- coding: utf-8 -*-
+"""
+Title: __________
+------------------------------------------------------------------------
+                         description
+------------------------------------------------------------------------
+Created: Sat Mar  4 16:45:37 2023
+Author: Robin de Freitas
+UID: 10887826
+Contact: robin.defreitas@student.manchester.ac.uk
+"""
+
+import numpy as np
+from initialise_model import initialise_ones_state, initialise_random_state
+from new_state_generation import calculate_dE_of_spin, choose_spins_to_flip
+from observables import calculate_state_energy, calculate_state_magnetisation
+from numpy import random
+from show_state import plot_state
+from print_formats import pvar
+from colorama import Style, Fore
+
+
+# LENGTH = 25
+# TEMPERATURE = 2
+# J_CONST = 1
+# NUMBER_OF_ITERATIONS = 1000
+
+
+def identify_neighbours(spin_coordinate, length):
+    i = spin_coordinate[0]
+    j = spin_coordinate[1]
+    
+    left = (i - 1) % length
+    right = (i + 1) % length
+    up = (j - 1) % length
+    down = (j + 1) % length
+    return {(left, j), (right, j), (i, up), (i, down)}
+
+
+def decide_to_flip_spin(spin_coordinate, spin_coordinate_neighbour, spins_state, J, probability_of_no_flip):
+    spin = spins_state[spin_coordinate[0], spin_coordinate[1]]
+    spin_neighbour = spins_state[spin_coordinate_neighbour[0], spin_coordinate_neighbour[1]]
+    if spin != spin_neighbour:
+        return False
+    random_number = random.rand()
+    if random_number < probability_of_no_flip:
+        return True
+    return False
+
+
+def branch_cluster_from_point(spin_coordinate, spins_state, cluster_coordinates, J, probability_of_no_flip):
+    neighbours = identify_neighbours(spin_coordinate, len(spins_state))
+    possible_coordinates = neighbours - cluster_coordinates
+    new_cluster_coordinates = set()
+    for coordinate in possible_coordinates:
+        flip_spin = decide_to_flip_spin(spin_coordinate, coordinate, spins_state, J, probability_of_no_flip)
+        if flip_spin:
+            new_cluster_coordinates.add(coordinate)
+    return new_cluster_coordinates
+
+
+def flip_cluster(spins_state, cluster_coordinates):
+    new_state = np.copy(spins_state)
+    for coordinate in cluster_coordinates:
+        new_state[coordinate] *= -1
+    return new_state
+
+
+def find_new_state_from_cluster_point(spins_state, spin_coordinate, J, T):
+    probability_of_no_flip = 1 - np.exp(-2 / T)
+    cluster_coordinates = {spin_coordinate}
+    tested_coordinates = set()
+    coordinates_to_test = cluster_coordinates - tested_coordinates
+    count = 0
+    while coordinates_to_test:
+        for spin_coordinate in coordinates_to_test:
+            new_cluster_coordinates = branch_cluster_from_point(spin_coordinate, spins_state, cluster_coordinates, J, probability_of_no_flip)
+            cluster_coordinates = cluster_coordinates.union(new_cluster_coordinates)
+            tested_coordinates.add(spin_coordinate)
+        coordinates_to_test = cluster_coordinates - tested_coordinates
+        
+        new_state = flip_cluster(spins_state, cluster_coordinates)
+        # plot_state(new_state, 'flipped') #cluster forming
+        count += 1
+    print("Cluster size: {}".format(len(cluster_coordinates)))
+    plot_state(new_state, 'New state') #whole cluster
+    return new_state
+
+
+def find_cluster_progression(state, temperature, length, number_of_iterations, J):
+    # initial_state = initialise_random_state(length)
+    # plot_state(initial_state, "initial")
+    spin_positions = choose_spins_to_flip(length, number_of_iterations)
+    new_state = state
+    energies = np.zeros(number_of_iterations)
+    magnetisations = np.zeros(number_of_iterations)
+    for count, spin_position in enumerate(spin_positions):
+
+        spin_coordinate = (spin_position[0], spin_position[1])
+        new_state = find_new_state_from_cluster_point(new_state, spin_coordinate, J, temperature)
+        print(Style.BRIGHT + Fore.RED + 'Iteration: {}'.format(count))
+        print(Style.BRIGHT + Fore.RED + 'Temperature: {}'.format(temperature))        
+        print(Style.RESET_ALL)
+        # plot_state(new_state, count)
+        print('\n')
+        
+        state_energy = calculate_state_energy(J, new_state)
+        energies[count] = state_energy
+        
+        state_magnetisation = calculate_state_magnetisation(new_state)
+        magnetisations[count] = state_magnetisation
+    
+    total_magnetisation = np.sum(magnetisations)
+    total_magnetisation_abs = np.sum(abs(magnetisations))
+    total_magnetisation_squared = np.sum(magnetisations**2)
+    total_magnetisation_4 = np.sum(magnetisations**4)
+    
+    total_energy = np.sum(energies)
+    total_energy_squared = np.sum(energies**2)
+    
+    
+    # pvar(energies)
+    # pvar(magnetisations)
+    
+    
+    # new_state = find_new_state_from_cluster_point(initial_state, spin_coordinate, J_CONST, TEMPERATURE)
+    pvar(new_state)
+    pvar(total_magnetisation)
+    pvar(total_magnetisation_abs)
+    pvar(total_magnetisation_squared)
+    pvar(total_magnetisation_4)
+    pvar(total_energy)
+    pvar(total_energy_squared)
+    return new_state, total_magnetisation, total_magnetisation_abs, total_magnetisation_squared, total_magnetisation_4, total_energy, total_energy_squared
+
+        # magnetisation += [total_magnetisation]
+        # magnetisation_abs += [total_magnetisation_abs]
+        # magnetisation_squared += [total_magnetisation_squared]
+        # magnetisation_4 += [total_magnetisation_4]
+        
+        # energy += [total_energy]
+        # energy_squared += [total_energy_squared]
+        # temperature_range += [temperature]
+
+
